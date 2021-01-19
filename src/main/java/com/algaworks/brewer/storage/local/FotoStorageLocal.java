50b0d5bfd2483ae77b9848a915a1d9aefc24a432
Rodrigo-Abreuu <77403600+Rodrigo-Abreuu@users.noreply.github.com>
2021-01-19 14:07:41 -0300

    Mostrando a foto na tela

--- src/main/java/com/algaworks/brewer/storage/local/FotoStorageLocal.java
+++ src/main/java/com/algaworks/brewer/storage/local/FotoStorageLocal.java
@@ -44,6 +44,15 @@ public class FotoStorageLocal implements FotoStorage {
 		}
 			return novoNome;
 	}
+
+	@Override
+	public byte[] recuperarFotoTemporaria(String nome) {
+		try {
+			return Files.readAllBytes(this.localTemporario.resolve(nome));
+		} catch (IOException e) {
+			throw new RuntimeException("Erro lendo a foto temporaria", e);
+		}
+	}
 	
 	private void criarPastas() {
 		try {
@@ -68,5 +77,7 @@ public class FotoStorageLocal implements FotoStorage {
 		}
 		return novoNome;
  	}
+
+
 	
 }
