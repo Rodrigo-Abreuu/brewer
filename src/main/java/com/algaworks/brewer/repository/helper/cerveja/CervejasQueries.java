58f3607bd6b9bae485c4be671b98a7d4882cd3fc
Rodrigo-Abreuu <77403600+Rodrigo-Abreuu@users.noreply.github.com>
2021-01-20 15:13:40 -0300

    Filtrando e resolvendo o problema do n mais 1

---
+++ src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasQueries.java
@@ -0,0 +1,13 @@
+package com.algaworks.brewer.repository.helper.cerveja;
+
+import java.util.List;
+
+import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.repository.filter.CervejaFilter;
+
+public interface CervejasQueries {
+
+	public List<Cerveja> filtrar(CervejaFilter filtro);
+	
+	
+}
