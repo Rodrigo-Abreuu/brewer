24e3c21d1b8764fdbe47e30dc7fa98393ed186f5
Rodrigo-Abreuu <77403600+Rodrigo-Abreuu@users.noreply.github.com>
2021-01-25 17:33:47 -0300

    Finalizando pesquisa de clientes

--- src/main/java/com/algaworks/brewer/model/Endereco.java
+++ src/main/java/com/algaworks/brewer/model/Endereco.java
@@ -8,7 +8,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Transient;
 
 @Embeddable
-public class Endereco implements Serializable{
+public class Endereco implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -26,43 +26,60 @@ public class Endereco implements Serializable{
 	
 	@Transient
 	private Estado estado;
-		
-	public Estado getEstado() {
-		return estado;
-	}
-	public void setEstado(Estado estado) {
-		this.estado = estado;
-	}
+
 	public String getLogradouro() {
 		return logradouro;
 	}
+
 	public void setLogradouro(String logradouro) {
 		this.logradouro = logradouro;
 	}
+
 	public String getNumero() {
 		return numero;
 	}
+
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
+
 	public String getComplemento() {
 		return complemento;
 	}
+
 	public void setComplemento(String complemento) {
 		this.complemento = complemento;
 	}
+
 	public String getCep() {
 		return cep;
 	}
+
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
+
 	public Cidade getCidade() {
 		return cidade;
 	}
+
 	public void setCidade(Cidade cidade) {
 		this.cidade = cidade;
 	}
-	 
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+	
+	public String getNomeCidadeSiglaEstado() {
+		if(this.cidade != null){
+			return this.cidade.getNome() + "/" + this.cidade.getEstado().getSigla();
+		}
+		return null;
+	}
 	
 }
