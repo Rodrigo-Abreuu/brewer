fc5647e69acdf47ae1e57948b0d714d7cf5621e1
Rodrigo-Abreuu <77403600+Rodrigo-Abreuu@users.noreply.github.com>
2021-01-26 12:31:35 -0300

    Implementando pesquisa de cidades

--- src/main/java/com/algaworks/brewer/repository/Cidades.java
+++ src/main/java/com/algaworks/brewer/repository/Cidades.java
@@ -7,11 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.brewer.model.Cidade;
 import com.algaworks.brewer.model.Estado;
+import com.algaworks.brewer.repository.helper.cidade.CidadesQueries;
 
-public interface Cidades extends JpaRepository<Cidade, Long>{
-	
+public interface Cidades extends JpaRepository<Cidade, Long>, CidadesQueries {
+
 	public List<Cidade> findByEstadoCodigo(Long codigoEstado);
 
 	public Optional<Cidade> findByNomeAndEstado(String nome, Estado estado);
-
+	
 }
