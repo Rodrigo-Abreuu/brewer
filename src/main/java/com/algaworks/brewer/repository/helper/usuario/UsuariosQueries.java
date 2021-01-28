190d9e9b2bedec5fa8195b2669c2d47f27b3b074
Rodrigo-Abreuu <77403600+Rodrigo-Abreuu@users.noreply.github.com>
2021-01-28 14:46:52 -0300

    Carregando as permissões do usuário

--- src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosQueries.java
+++ src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosQueries.java
@@ -1,5 +1,6 @@
 package com.algaworks.brewer.repository.helper.usuario;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.algaworks.brewer.model.Usuario;
@@ -8,4 +9,5 @@ public interface UsuariosQueries {
 
 	public Optional<Usuario> porEmailEAtivo(String email);
 	
+	public List<String> permissoes(Usuario usuario);
 }
--- src/main/java/com/algaworks/brewer/security/AppUserDetailsService.java
+++ src/main/java/com/algaworks/brewer/security/AppUserDetailsService.java
@@ -1,9 +1,14 @@
 package com.algaworks.brewer.security;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -22,10 +27,17 @@ public class AppUserDetailsService implements UserDetailsService{
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		Optional<Usuario> usuarioOptional = usuarios.porEmailEAtivo(email);
-		
 		Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
+		return new User(usuario.getEmail(), usuario.getSenha(), getPermissao(usuario));
+	}
+
+	private Collection<? extends GrantedAuthority> getPermissao(Usuario usuario) {
+		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+ 		
+		List<String> permissoes = usuarios.permissoes(usuario);
+		permissoes.forEach(p -> authorities.add(new SimpleGrantedAuthority(p.toUpperCase())));
 		
-		return new User(usuario.getEmail(), usuario.getSenha(), new HashSet<>());
+		return authorities;
 	}
 	
 
