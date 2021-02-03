0b4eea1e97e75cb86c2343c9499b956241b955c5
Rodrigo-Abreuu <77403600+Rodrigo-Abreuu@users.noreply.github.com>
2021-02-03 17:23:17 -0300

    Mantendo os itens da venda no servidor

--- target/classes/static/javascripts/venda.autocomplete-itens.js
+++ target/classes/static/javascripts/venda.autocomplete-itens.js
@@ -4,9 +4,10 @@ Brewer.Autocomplete = (function() {
 
 	function Autocomplete() {
 		this.skuOuNomeInput = $('.js-sku-nome-cerveja-input');
-		var htmlTemplateAutocomplete = $('#template-autocomplete-cerveja')
-				.html();
+		var htmlTemplateAutocomplete = $('#template-autocomplete-cerveja').html();
 		this.template = Handlebars.compile(htmlTemplateAutocomplete);
+		this.emitter = $({});
+		this.on = this.emitter.on.bind(this.emitter); 
 	}
 
 	Autocomplete.prototype.iniciar = function() {
@@ -22,17 +23,25 @@ Brewer.Autocomplete = (function() {
 			},
 			template : {
 				type : 'custom',
-				method : function(nome, cerveja) {
-					cerveja.valorFormatado = Brewer
-							.formatarMoeda(cerveja.valor);
-					return this.template(cerveja);
-				}.bind(this)
+				method : template.bind(this)
+			},
+			list: {
+				onChooseEvent: onItemSelecionado.bind(this)
 			}
 		};
 
 		this.skuOuNomeInput.easyAutocomplete(options);
 	}
 
+	function onItemSelecionado(){
+		this.emitter.trigger('item-selecionado', this.skuOuNomeInput.getSelectedItemData());
+	}
+	
+	function template(nome, cerveja){
+		cerveja.valorFormatado = Brewer.formatarMoeda(cerveja.valor);
+		return this.template(cerveja);
+	}
+	
 	return Autocomplete
 
 }());
