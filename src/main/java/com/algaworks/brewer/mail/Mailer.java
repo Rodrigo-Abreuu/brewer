a01915563de467ca64452115aa78e73e8a3ca832
Rodrigo-Abreuu <77403600+Rodrigo-Abreuu@users.noreply.github.com>
2021-02-09 16:08:34 -0300

    Criando template para e-mail

--- src/main/java/com/algaworks/brewer/mail/Mailer.java
+++ src/main/java/com/algaworks/brewer/mail/Mailer.java
@@ -1,15 +1,28 @@
 package com.algaworks.brewer.mail;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import com.algaworks.brewer.model.Venda;
 
 @Component
 public class Mailer {
 
-	@Async
-	public void enviar(){
-
+	@Autowired
+	private JavaMailSender mailSender;
 	
+	@Async
+	public void enviar(Venda venda){
+		SimpleMailMessage mensagem = new SimpleMailMessage();
+		mensagem.setFrom("rodrigo.moura.abreu06@aluno.ifce.edu.br");
+		mensagem.setTo(venda.getCliente().getEmail());
+		mensagem.setSubject("Venda efetuada");
+		mensagem.setText("Obrigado, sua venda foi processada!");
+		
+		mailSender.send(mensagem);
 	}
 	
 }
